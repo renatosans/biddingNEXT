4af9d0676d69f5c76f009332983b7955dfede6d8
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-09-29 20:12:38 -0300

    Ajustes

---
+++ pages/api/contractors.js
@@ -0,0 +1,27 @@
+import prisma from '../../config/db'
+
+
+const getContractors = (req, res) => {
+	prisma.contractor.findMany()
+	.then((contractors) => res.send(contractors))
+	.catch((error) => res.send("Error: " + error.message))
+}
+
+const insertContractor = (req, res) => {
+	const { id, companyName, contactPerson, email } = req.body;
+
+	prisma.contractor.create({ data: req.body })
+	.then((result) => res.send(result))
+	.catch((error) => res.send("Error: " + error.message))
+}
+
+module.exports = {
+	default: (req, res) => {
+		switch (req.method) {
+			case "GET": return getContractors(req, res)
+			case "POST": return insertContractor(req, res)
+		}
+	},
+    get: (req, res) => getContractors(req, res),
+    post: (req, res) => insertContractor(req, res)
+}
