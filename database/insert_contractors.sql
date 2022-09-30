a873314262aae79b6af6e089c37a97b0d8b57418
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-09-30 08:45:39 -0300

    Inclusão dos scripts de banco de dados

---
+++ database/insert_contractors.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO contractor ("companyName", "contactPerson", email) VALUES ('Banana Company','Amelia','amelia@banana.com');
+INSERT INTO contractor ("companyName", "contactPerson", email) VALUES ('Orange Company','Osvald','osvald@orange.com');
+INSERT INTO contractor ("companyName", "contactPerson", email) VALUES ('Apple Company','Steve','steve@apple.com');
+INSERT INTO contractor ("companyName", "contactPerson", email) VALUES ('Lemon Company','Harry','harry@lemon.com');
---
+++ database/insert_items.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO item (name, "avgPrice", "unitOfMeasurement", "itemGroup") VALUES ('Terraplanagem',9600,'MQ',NULL);
+INSERT INTO item (name, "avgPrice", "unitOfMeasurement", "itemGroup") VALUES ('Acabamentos e revestimentos',5700,'UN',NULL);
+INSERT INTO item (name, "avgPrice", "unitOfMeasurement", "itemGroup") VALUES ('Supervisão de obras',3400,'HS',NULL);
+INSERT INTO item (name, "avgPrice", "unitOfMeasurement", "itemGroup") VALUES ('Escavação para fundações',9800,'MQ',NULL);
