443d79749b0c485ce87e498ede0e0eaf5654e640
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-09-30 23:36:18 -0300

    Documentação

--- database/insert_contractors.sql
+++ database/insert_contractors.sql
@@ -1,4 +1,8 @@
 
+-- -------------------------------------------------------------
+-- For PostgreSQL keep the double quotes, for MySQL remove them
+-- -------------------------------------------------------------
+
 INSERT INTO contractor ("companyName", "contactPerson", email) VALUES ('Banana Company','Amelia','amelia@banana.com');
 INSERT INTO contractor ("companyName", "contactPerson", email) VALUES ('Orange Company','Osvald','osvald@orange.com');
 INSERT INTO contractor ("companyName", "contactPerson", email) VALUES ('Apple Company','Steve','steve@apple.com');
--- database/insert_items.sql
+++ database/insert_items.sql
@@ -1,4 +1,8 @@
 
+-- -------------------------------------------------------------
+-- For PostgreSQL keep the double quotes, for MySQL remove them
+-- -------------------------------------------------------------
+
 INSERT INTO item (name, "avgPrice", "unitOfMeasurement", "itemGroup") VALUES ('Terraplanagem',9600,'MQ',NULL);
 INSERT INTO item (name, "avgPrice", "unitOfMeasurement", "itemGroup") VALUES ('Acabamentos e revestimentos',5700,'UN',NULL);
 INSERT INTO item (name, "avgPrice", "unitOfMeasurement", "itemGroup") VALUES ('Supervisão de obras',3400,'HS',NULL);
