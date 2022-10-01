6b904d95c3588df3c62739db63b7cf063dad2e3c
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-09-30 21:20:47 -0300

    Ajustes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import useSWR from 'swr'
 import axios from 'axios'
-import { request, gql} from 'graphql-request'
 import ItemCard from '../components/ItemCard'
-import styles from '../styles/Home.module.css'
 
 
 const url = '/api/graphql';
