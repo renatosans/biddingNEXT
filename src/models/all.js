1e842654e56905890891b395911eef69f39e5b86
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-10-16 16:49:01 -0300

    Correção de bug de override dos resolvers

--- src/models/all.js
+++ src/models/all.js
@@ -2,8 +2,13 @@ import { typeDefs as itemTypes, resolvers as itemResolvers } from './item';
 import { typeDefs as contractorTypes, resolvers as contractorResolvers } from './contractor';
 
 
-const typeDefs = [ `type Query { version: String }`, contractorTypes, itemTypes]
-const resolvers = Object.assign({}, contractorResolvers, itemResolvers)
+const typeDefs = [ `type Query { version: String }`, itemTypes, contractorTypes ]
 
+// Object.assign( ) não é recursivo como o lodash.merge( )  e isso causa o override dos resolvers
+// a solução foi usar o Object.assign( ) para Query e Mutation separadamente
+const resolvers = {
+    Query: Object.assign({}, itemResolvers.Query, contractorResolvers.Query),
+    Mutation: Object.assign({}, itemResolvers.Mutation, contractorResolvers.Mutation)
+}
 
 export { typeDefs, resolvers }
