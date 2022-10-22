e09dafc9fb43759d83fbb1e856f9e937c8ac0c26
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-10-22 11:12:37 -0300

    Ajustes

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ export default function Home() {
       <h1>HOME</h1>
       <h2>Versão oficial(frontend e backend separados) no endereço: </h2>
       <p>https://github.com/renato-sanseverino/InvitationForBid.git</p>
+
+      <p>Esta versão é um protótipo. Possui alguma lentidão
+        devido ao framework Next.js e ao Apollo Server</p>
     </Layout>
   )
 }
